9ce96a30683ed566e85f2cfca2d2d14e6f2345eb
gmbreguet <guy-michel.breguet@heig-vd.ch>
2022-03-01 17:32:39 +0100

    update GMB

--- 2022-03-01.cpp
+++ 2022-03-01.cpp
@@ -1,12 +1,26 @@
 #include <iostream>
 #include <cstdlib>
+#include <vector>
 
 using namespace std;
 
+
 //------------------------------------------------------
-// main
+template<typename Iterator>
+void f(Iterator& it) {
+   cout << (*it) << endl;
+   ++it;
+   cout << (*it) << endl;
+}
 //------------------------------------------------------
 int main () {
+
+   vector<int> v = {1, 2, 3, 4};
+   vector<int>::iterator it = v.begin();
+   f(it);
+   cout << (*it) << endl;
+
+//   f(4);
 
    return EXIT_SUCCESS;
 }  // main
