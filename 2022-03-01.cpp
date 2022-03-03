0ee2d8c3e9367c6465cccd2b71cd29860afbc10f
gmbreguet <guy-michel.breguet@heig-vd.ch>
2022-03-03 11:28:07 +0100

    update GMB

--- 2022-03-01.cpp
+++ 2022-03-01.cpp
@@ -5,21 +5,18 @@
 using namespace std;
 
 //------------------------------------------------------
-// https://stackoverflow.com/questions/20244743/pass-iterator-as-a-function-parameter
-template<typename Iterator>
-void f(Iterator& it) {
-   cout << (*it) << endl;
-   ++it;
-   cout << (*it) << endl;
+void f(int i) {
+   cout << i << endl;
+   if (i == 0) // cas trivial
+      return;
+   f(i-1);  // i => cas trivial
+   cout << i << endl;
 }
 
 //------------------------------------------------------
 int main () {
 
-   vector<int> v = {1, 2, 3, 4};
-   vector<int>::iterator it = v.begin();
-   f(it);
-   cout << (*it) << endl;
+   f(3);
 
    return EXIT_SUCCESS;
 }  // main
---
+++ 2022-03-03.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+
+using namespace std;
+
+//------------------------------------------------------
+// https://stackoverflow.com/questions/20244743/pass-iterator-as-a-function-parameter
+template<typename Iterator>
+void f(Iterator it) {
+   cout << (*it) << endl;
+   ++it;
+   cout << (*it) << endl;
+}
+
+//------------------------------------------------------
+int main () {
+
+   vector<int> v = {1, 2, 3, 4};
+   vector<int>::iterator it = v.begin();
+   f(it);
+   cout << (*it) << endl;
+
+   return EXIT_SUCCESS;
+}  // main
+
